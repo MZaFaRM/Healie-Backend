f49c3ae1fe9f23c44cae0fe057f6e9dfec0d2323
MZaFaRM <muhammedzafar.mm@gmail.com>
2024-02-03 03:59:47 +0530

    [FEAT] treatment, summarize symptoms

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List
 import requests
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -5,13 +6,14 @@ from decouple import config
 
 app = FastAPI()
 
+
 class ChatRequest(BaseModel):
     prompt: str
     previous_history: list = []
 
 
 @app.post("/diagnose/")
-async def read_root(chat_request: ChatRequest):
+async def diagnose_user(chat_request: ChatRequest):
     headers = {
         "Authorization": f"Bearer {config('API_TOKEN')}",
         "Content-Type": "application/json; charset=utf-8",
@@ -20,14 +22,21 @@ async def read_root(chat_request: ChatRequest):
     url = "https://api.edenai.run/v2/text/chat"
     payload = {
         "providers": "openai",
-        "text": chat_request.prompt + ".  Provide a follow up question for diagnosis",
+        "text": (
+            chat_request.prompt +
+            ". Provide a follow up question for diagnosis to "
+            "gather more about the issue (lifestyle or "
+            "genetics or previous treatment responses etc.)"
+        ),
         "chat_global_action": (
             "Act as Indian medical LLM and Provide personalized health advice based on user input. "
             "Ask follow-up questions to gather comprehensive details "
-            "about symptoms, medical history, and lifestyle. Reduce possibly bias questions."
+            "about symptoms, medical history, and lifestyle. Reduce possibly bias questions. "
+            "If you have got from previous_history and user input sufficient "
+            "information provide a diagnosis and treatment plan else "
         ),
         "previous_history": chat_request.previous_history,
-        "temperature": 0.0,
+        "temperature": 0.8,
         "max_tokens": 150,
     }
 
@@ -37,7 +46,7 @@ async def read_root(chat_request: ChatRequest):
     return {"response": result}
 
 
-class UserHealth(BaseModel):
+class ChatHistory(BaseModel):
     blood_pressure: str  # Usually captured as a string to include both systolic and diastolic values, e.g., "120/80"
     total_cholesterol: int  # Measured in mg/dL
     blood_sugar: int  # Blood glucose levels in mg/dL
@@ -50,31 +59,68 @@ class UserHealth(BaseModel):
     bone_density: float  # g/cm², but the representation might vary
 
 
-# @app.post("/summarize/")
-# async def read_root(user_health: UserHealth):
-       
+@app.post("/summarize/")
+async def summarize_health(user_health: ChatHistory):
+    report = " - ".join(item + ":" + str(value) for item, value in user_health)
+
+    headers = {
+        "Authorization": f"Bearer {config('API_TOKEN')}",
+        "Content-Type": "application/json; charset=utf-8",
+    }
+
+    url = "https://api.edenai.run/v2/text/chat"
+    payload = {
+        "providers": "openai",
+        "text": (
+            report + "\n\n Analyse the given data and"
+            " provide a general overview of my health within a sentence."
+        ),
+        "chat_global_action": (
+            "Act as Indian medical LLM and provide a concise response."
+        ),
+        "previous_history": [],
+        "temperature": 0.8,
+        "max_tokens": 100,
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    result = response.json()
+
+    return {"response": result}
+
+
+class ChatHistory(BaseModel):
+    history: List[dict]
+
+
+@app.post("/treatment/")
+async def provide_treatment(chat_history: ChatHistory):
+    data = "\n".join(
+        f"{convo["role"]}: {convo["message"]}" for convo in chat_history.history
+    )
     
+    headers = {
+        "Authorization": f"Bearer {config('API_TOKEN')}",
+        "Content-Type": "application/json; charset=utf-8",
+    }
+
+    url = "https://api.edenai.run/v2/text/chat"
+    payload = {
+        "providers": "openai",
+        "text": (
+            "Analyse the given data and provide a "
+            "treatment plan for the user in one paragraph.\n\n" + data
+        ),
+        "chat_global_action": (
+            "Act as Indian medical LLM and provide a concise response."
+        ),
+        "previous_history": [],
+        "temperature": 0.8,
+        "max_tokens": 180,
+    }
+
+    data = requests.post(url, json=payload, headers=headers)
+    result = data.json()
+
+    return {"response": result}
     
-    # headers = {
-    #     "Authorization": f"Bearer {config('API_TOKEN')}",
-    #     "Content-Type": "application/json; charset=utf-8",
-    # }
-
-    # url = "https://api.edenai.run/v2/text/chat"
-    # payload = {
-    #     "providers": "openai",
-    #     "text": chat_request.prompt + ".  Provide a follow up question for diagnosis",
-    #     "chat_global_action": (
-    #         "Act as Indian medical LLM and Provide personalized health advice based on user input. "
-    #         "Ask follow-up questions to gather comprehensive details "
-    #         "about symptoms, medical history, and lifestyle. Reduce possibly bias questions."
-    #     ),
-    #     "previous_history": chat_request.previous_history,
-    #     "temperature": 0.0,
-    #     "max_tokens": 150,
-    # }
-
-    # response = requests.post(url, json=payload, headers=headers)
-    # result = response.json()
-
-    # return {"response": result}
